b312daf2c3456cc749c0d15e2752b2e2436ea3bf
Arno T <captaincoyote10@gmail.com>
2024-10-29 07:30:02 +0800

    Setup dag

---
+++ airflow/dags/air_quality_etl.py
@@ -0,0 +1,152 @@
+from airflow.decorators import dag, task
+from airflow.utils.dates import days_ago
+import logging
+
+logger = logging.getLogger('airflow.task')
+
+@task()
+def get_available_stations(**context) -> list[dict]:
+    from airflow.providers.http.hooks.http import HttpHook
+    from airflow.models import Variable
+    from requests import Response
+    import json
+
+    # Set up hook
+    waqi_api_token = Variable.get("waqi_api_token")
+    httphook = HttpHook(
+        method='GET',
+        http_conn_id='waqi_api',
+    )
+
+    # Send request to API
+    lat_long_bounds = '-10.6,113.2,-43.6,153.6' # Bounds for Australia
+    logger.info(f"Geo-coordinates (lat1, long1, lat2, long2): {lat_long_bounds}")
+    response: Response = httphook.run(
+        endpoint=f'/map/bounds?latlng={lat_long_bounds}&token={waqi_api_token}',
+    )
+
+    # Process response
+    logger.debug(f'Response status: {response.status_code}')
+    if not response.ok:
+        response.raise_for_status()
+    data = json.loads(response.text)
+
+    # Process response data
+    stations = data['data']
+    logger.info(f'Stations: {stations}')
+    stations_filtered = [
+        {'name':station['station']['name'],
+         'id':station['uid']}
+        for station in stations
+    ]
+    logger.info(f'Found {len(stations_filtered)} stations in Australia')
+    return stations_filtered
+
+@task()
+def get_station_data(stations: list[dict], **context) -> list[dict]:
+    from airflow.providers.http.hooks.http import HttpHook
+    from airflow.models import Variable
+    from requests import Response
+    import json
+
+    # Set up hook
+    waqi_api_token = Variable.get("waqi_api_token")
+    httphook = HttpHook(
+        method='GET',
+        http_conn_id='waqi_api',
+    )
+
+    # Retrieve raw data from API and store raw data
+    stations_data = []
+    logger.info(f'Retrieving data for {len(stations)} stations')
+    for station in stations:
+        logger.info(f'::group::Retrieving data for {station["name"]}')
+        endpoint = f'/feed/@{station["id"]}/?token={waqi_api_token}'
+        logger.info(f'Endpoint: {endpoint}')
+
+        # Send request to API
+        response: Response = httphook.run(endpoint=endpoint)
+        logger.debug(f'Response status: {response.status_code}')
+        if not response.ok:
+            response.raise_for_status()
+        data = json.loads(response.text)
+        logger.info(f'Response data: {data}')
+        
+        # Check response status
+        if 'data' not in data.keys() or 'status' not in data.keys():
+            logger.error(f'No status or data in response. Response: {data}')
+            logger.info('::endgroup::')
+            continue
+        if data['status'] != 'ok':
+            logger.error(f'Could not find station. Reason: {data['data']}')
+            logger.info('::endgroup::')
+            continue
+        
+        # Store station data
+        station = data['data']
+        logger.info(f'Station: {station}')
+        stations_data.append(station)
+        logger.info('::endgroup::')
+
+    logger.info(f'Retrieved data for {len(stations_data)} of {len(stations)} stations')
+    return stations_data
+
+@task()
+def process_air_quality_data(stations_data: list[dict], **context):
+    refined_data = []
+    num_stations = len(stations_data)
+    logger.info(f'Processing data for {num_stations} stations')
+    for idx, station_data in enumerate(stations_data):
+        logger.info(f'::group::Processing station {idx+1}/{num_stations}')
+        
+        # Get the timestamp of when the data was collected in iso format
+        time_data: dict = station_data.get('time')
+        date_iso_str = time_data.get('iso') if time_data else None
+
+        # Refine the data to only the datapoints we want
+        refined_data.append({
+            'station_id': station_data.get('idx'),
+            'station_name': station_data['city'].get('name'),
+            'dominant_pollutant': station_data.get('dominentpol'),
+            'aqi': station_data.get('aqi'),
+            'data_timestamp': date_iso_str
+        })
+        logger.info(f'Refined station data: {refined_data[-1]}')
+        logger.info('::endgroup::')
+    return refined_data
+
+@task()
+def load_dwh(transformed_data: list[dict], **context):
+    from src.clickhouse import get_clickhouse_con
+    con = get_clickhouse_con()
+
+    aqi_table = 'air_quality'
+    aq_table = con.table(aqi_table)
+    logger.info(aq_table.execute())
+
+    logger.info(f"Inserting into '{aqi_table}': {transformed_data}")
+    con.insert(aqi_table, transformed_data)
+    logger.info(aq_table.execute())
+
+# Define the Airflow DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+}
+
+
+@dag(
+    dag_id='waqi_air_quality_etl',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False
+)
+def etl_dag():
+    station_names = get_available_stations()
+    stations_data_raw = get_station_data(station_names)
+    stations_data_transformed = process_air_quality_data(stations_data_raw)
+
+    station_names >> stations_data_raw >> stations_data_transformed >> load_dwh(stations_data_transformed)
+    
+
+etl_dag()
---
+++ airflow/dags/src/clickhouse.py
@@ -0,0 +1,17 @@
+from airflow.models import Variable
+import ibis
+import ibis.backends
+import ibis.backends.clickhouse
+
+def get_clickhouse_con() -> ibis.backends.clickhouse.Backend:
+    clickhouse_host = Variable.get("clickhouse_host")
+    clickhouse_port = Variable.get("clickhouse_port")
+    clickhouse_user = Variable.get("clickhouse_user")
+    clickhouse_pwd = Variable.get("clickhouse_pword")
+    con = ibis.clickhouse.connect(
+        host=clickhouse_host,
+        port=clickhouse_port,
+        user=clickhouse_user,
+        password=clickhouse_pwd
+    )
+    return con
