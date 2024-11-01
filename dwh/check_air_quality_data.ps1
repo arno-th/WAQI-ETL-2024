0d7d452a2c79226963a4ee9ba0d02ffecf613d7f
Arno T <captaincoyote10@gmail.com>
2024-11-01 16:13:04 +0800

    Update data check script

--- dwh/check_air_quality_data.ps1
+++ dwh/check_air_quality_data.ps1
@@ -1,2 +1,2 @@
 # Run this with `./check_air_quality_data.ps1` from a shell terminal in `dwh` folder
-Get-Content .\check_air_quality_data.sql | docker exec -i clickhouse clickhouse-client
+Get-Content .\air_quality_data.sql | docker exec -i clickhouse clickhouse-client
